d8ca036554bc9604b4f58a8f8af7de1528773fba
AniModi <98632268+AniModi@users.noreply.github.com>
2024-06-03 11:02:24 +0530

    update : Update Section with additional details

--- src/texts/updates.tsx
+++ src/texts/updates.tsx
@@ -6,4 +6,7 @@ export const updates = [
   <>13th May, 2024 - Implemented core features of the new design </>,
   <>15th May, 2024 - Deployed with new design</>,
   <>21st May, 2024 - Moved hosting to firebase</>,
+  <>24th May, 2024 - Option to create learning maps using GPT-4</>,
+  <>27th May, 2024 - Added a prompt management map</>,
+  <>31st May, 2024 - Added intuitive error feedback</>,
 ];
